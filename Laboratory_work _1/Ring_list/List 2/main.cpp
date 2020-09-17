d95ad12a7e0330cd7e5bd361b9217841bc76f5cc
Oleg Oksenchuk <oleg.oksenchuk@mail.ru>
2020-09-17 23:13:30 +0300

    Laboratory_work_1 2 part(ring_list) is done.

--- Laboratory_work _1/Ring_list/List 2/main.cpp
+++ Laboratory_work _1/Ring_list/List 2/main.cpp
@@ -2,10 +2,39 @@
 #include <iostream>
 
 int main() {
+
 	using namespace std;
+	using namespace rng;
+	setlocale(LC_ALL, "Russian");
+	uint interval;
+	cin >> interval;
 
-	ring_list list;
-	list.create(10);
+	ring_list* list = (ring_list*) new ring_list;
 
+	for (uint i = 1; i <= 64; i++)
+	{
+		list->create(i);
+
+		uint counter = 0;
+		rng::iterator it = list->begin();
+
+		while (list->size() > 1) {
+
+			counter++;
+			if (counter > interval)counter = 1;
+
+			if (counter == interval) {
+				cout <<" "<< *it;
+				list->erase(it);
+			}
+			else
+				++it;
+		}
+
+		it = list->begin();
+		cout << ":   Номер последнего игрока:" << *it << endl << endl;
+		list->clear();
+	}
+	system("pause");
 	return 0;
 }
--- Laboratory_work _1/Ring_list/List 2/ring_list.h
+++ Laboratory_work _1/Ring_list/List 2/ring_list.h
@@ -2,10 +2,13 @@
 #ifndef _RING_LIST_H_
 #define _RING_LIST_H_
 
-class ring_list {
+namespace rng {
 
 	typedef unsigned int uint;
-	struct Element{
+
+
+	struct Element {
+
 		uint index;
 		Element* next;
 		Element(uint index_, Element* next_) :index(index_), next(next_) {
@@ -13,33 +16,109 @@ class ring_list {
 		}
 	};
 
-private:
 
-	Element* head_;
+	class iterator {
+	private:
 
-public:
+		Element* pointer_;
 
-	void create(const uint number_of_elements) {
+	public:
 
-		Element* ptr = head_;
-
-		for (uint i = 0; i < number_of_elements; i++) {
-
-			ptr->next = new Element(i + 1, nullptr);
-			ptr = ptr->next;
-
+		explicit iterator(Element* pointer) noexcept :pointer_(pointer) {
 
 		}
-	
-		ptr->next = head_->next;
+		void operator++() {
 
-	}
+			pointer_ = pointer_->next;
 
-	void clear() {
+		}
 
+		uint operator* () {
 
+			return (pointer_->next)->index;
 
-	}
+		}
+		inline Element* GetAdress()const noexcept{
+			return pointer_;
+		}
 
-};
+	};
+
+	class ring_list {
+
+	private:
+
+		uint number_of_elements_;
+		Element* head_;
+		Element* ass_;
+
+	public:
+
+		explicit ring_list()noexcept : head_(new Element(0, nullptr)), number_of_elements_(0) {
+
+		}
+
+		inline uint size()const noexcept {
+
+			return number_of_elements_;
+
+		}
+
+		void create(const uint number_of_elements) {
+
+			Element* ptr = head_;
+
+			for (uint i = 0; i < number_of_elements; i++) {
+
+				ptr->next = new Element(i + 1, nullptr);
+				ptr = ptr->next;
+
+			}
+
+			ptr->next = head_->next;
+			ass_ = ptr;
+			number_of_elements_ = number_of_elements;
+
+		}
+
+		void clear() {
+
+			if (number_of_elements_ > 0) {
+
+				Element* ptr_delete = head_->next, * ptr_next;
+				head_->next = nullptr;
+
+				for (uint i = 0; i < number_of_elements_; i++)
+				{
+					ptr_next = ptr_delete->next;
+					if (ptr_delete != nullptr)delete ptr_delete;
+					ptr_delete = ptr_next;
+				}
+
+			}
+
+			this->number_of_elements_ = 0;
+		}
+
+		void erase(iterator it) {
+
+			Element* ptr_previous = it.GetAdress();
+			Element* ptr_current = ptr_previous->next;
+			ptr_previous->next = ptr_current->next;
+			if (ptr_current == head_->next)head_->next = ptr_previous->next;
+			if (ptr_current == ass_)ass_ = ptr_previous;
+			delete ptr_current;
+			number_of_elements_--;
+
+		}
+
+		inline iterator begin()const noexcept {
+
+			return iterator(ass_);
+
+		}
+
+	};
+
+}
 #endif
