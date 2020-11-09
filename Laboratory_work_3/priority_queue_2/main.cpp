62deb2ee21a90dcd6dd08fd8f395ebcecf1f8465
Oleg Oksenchuk <oleg.oksenchuk@mail.ru>
2020-11-09 21:21:19 +0300

    Laboratory_work_3. Added table with missed and common tackts.

--- Laboratory_work_3/priority_queue_2/main.cpp
+++ Laboratory_work_3/priority_queue_2/main.cpp
@@ -13,15 +13,28 @@ void OutptTableHeader() {
 		std::cout << std::setw(6) << i << "|";
 
 	}
+
+	std::cout << "  INPUT TICKS";
+
 	std::cout << std::endl;
 	std::cout << kSeparatorLine << std::endl;
 }
+
+struct MissedCommon {
+
+	size_t misssed;
+	size_t common;
+
+};
+//
+MissedCommon info[10][10];
+//
 double StartSimulation(const size_t interval_ticks, const size_t input_ticks) {
 
 	priority_queue<Thread> queue;
 
 	const size_t input = input_ticks;
-
+	//My
 	queue.push({ { {3},{2},{1},{6},{3},{2},{5},{4} }, input, 1 }, 1);
 	queue.push({ { {2},{1},{2},{3},{3},{1},{2},{6},{4} }, input, 1 }, 1);
 	queue.push({ { {4},{1},{6},{8},{5},{4},{2},{3},{1},{2} }, input, 1 }, 1);
@@ -31,14 +44,34 @@ double StartSimulation(const size_t interval_ticks, const size_t input_ticks) {
 	queue.push({ { {3},{2},{3},{1},{3},{4},{1},{2},{1},{6},{1},{2} }, input, 3 }, 3);
 	queue.push({ { {2},{3},{3},{3},{1},{6},{3},{8},{4},{2},{2},{2} }, input,3 }, 3);
 
+	//Marian
+	/*queue.push({ { {2},{2},{3},{4},{2},{1},{1},{3},{3},{1} }, input, 1 }, 1);
+	queue.push({ { {4},{1},{1},{1},{1},{2},{2},{3},{1},{3},{3},{2} }, input, 1 }, 1);
+	queue.push({ { {2},{1},{3},{2},{4},{5},{4},{2},{2},{2} }, input, 1 }, 1);
+	queue.push({ { {4},{5},{4},{2},{3},{3},{3},{4},{3},{5},{3} }, input, 1 }, 1);
+	queue.push({ { {1},{4},{3},{5},{4},{2},{3},{2},{3} }, input, 2 }, 2);*/
+
+	//Desk test
+	/*queue.push({ { {4},{2},{3} }, input, 1 }, 1);
+	queue.push({ { {3},{4},{2} }, input, 2 }, 2);
+	queue.push({ { {3},{2},{3} }, input, 2 }, 2);*/
+
+
 	Cpu CPU(interval_ticks);
 
-	while (!queue.empty()) CPU << queue;
+	if (interval_ticks == 4 && input_ticks == 4)
+		size_t a = 10;
+	
+	while (!queue.empty()) 
+		CPU << queue;
 
 	const double kCommonTicks = static_cast<double>(CPU.get_common_ticks());
 	const double kMissedTicks = static_cast<double>(CPU.get_missed_ticks());
 	const double kUsefullTicks = kCommonTicks - kMissedTicks;
-	
+	//
+	info[interval_ticks - 1][input_ticks - 1].misssed = kMissedTicks;
+	info[interval_ticks - 1][input_ticks - 1].common = kCommonTicks;
+	//
 	return kUsefullTicks / kCommonTicks;
 }
 
@@ -67,8 +100,31 @@ int main() {
 
 	}
 
+	std::cout << "C\nP\nU\n \nT\nI\nC\nK\nS\n";
+	//
+
+	for (size_t interval_ticks = 1; interval_ticks <= 10; interval_ticks++) {
+
+		std::cout.width(7);
+		std::cout << interval_ticks << kVerticalSeparator;
+
+		for (size_t input_ticks = 1; input_ticks <= 10; input_ticks++) {
+
+			std::cout.width(7);
+			std::cout << info[interval_ticks - 1][input_ticks -1 ].misssed << kSpace << info[interval_ticks -1 ][input_ticks - 1].common << kSpace << kVerticalSeparator;
+
+		}
+
+		std::cout << std::endl;
+		std::cout << kSeparatorLine << kSeparatorLine;
+		std::cout << std::endl;
+
+	}
+
+	//
 	system("pause");
-	return std::cin.good();
+
+	return EXIT_SUCCESS;
 }
 
 
