ca5d6a235b79a5518f0ca0397c6ffe366142408d
Oleg Oksenchuk <oleg.oksenchuk@mail.ru>
2020-12-07 21:31:02 +0300

    lab work 5.

--- Laboratory_work_5/main.cpp
+++ Laboratory_work_5/main.cpp
@@ -1,17 +1,39 @@
 #include "BinaryTree.hpp"
+#include "LinkedBinaryTree.hpp"
 #include <iostream>
+#include <vector>
 
-void ShowVertex(const BinaryTree<int>::Vertex& vertex)noexcept(true) {
-
-
-
-}
 int main() {
 
 	setlocale(LC_ALL, "ru");
-	BinaryTree<int> btree{ 8, 12, 4, 6, 5, 3, 11, 9, 16 };
+	std::cout << "Введите числа: ";
 
-	btree.SetProcessVertexFunc([](const BinaryTree<int>::Vertex& vertex)->void { std::cout << "(" <<vertex.data_ << ") "; });
+	BinaryTree<int> btree;
+
+	bool is_input = true;
+
+	std::vector<int> input_list;//{5, 2, 6, 1, 4, 9, 7, 3};
+
+	while (is_input) {
+
+		int input = 0;
+		
+		std::cin >> input;
+
+		if (input == 0)
+			is_input = false;
+		else
+			//btree.Add(input);
+			input_list.push_back(input);
+	}
+
+	for (auto& obj : input_list)
+		btree.Add(obj);
+
+	std::cout << std::endl;
+	btree.print_tree();
+
+	btree.SetProcessVertexFunc([](const BinaryTree<int>::Vertex& vertex)->void { std::cout << "(" <<vertex.data_ << ")"; });
 
 	std::cout << "Прямой обход:" << std::endl;
 	btree.TraversePreorder();
@@ -24,6 +46,37 @@ int main() {
 	std::cout << "Симметричный обход:" << std::endl;
 	btree.TraverseInorder();
 	std::cout << std::endl;
+
+	LinkedBinaryTree<int> linkedBtree{};
+
+	for (auto& obj : input_list)
+		linkedBtree.Add(obj);
+
+	std::cout << "Прошитое бинарное дерево: " << std::endl;
+	linkedBtree.print_tree();
+
+	bool is_delete = true;
+
+	while (is_delete) {
+
+		int input = 0;
+
+		std::cout << "Введите элемент, который нужно удалить: ";
+		std::cin >> input;
+
+		if (input == 0)
+			is_delete = false;
+		else {
+
+			linkedBtree.Delete(input);
+			std::cout << "Дерево после удаления: " << std::endl;
+			linkedBtree.print_tree();
+			std::cout << std::endl;
+
+		}
+
+	}
+	
 
 	return 0;
 }
